64ed8656c900298cd49048bb69c75804c05b3b86
Dmitriy Kochurov <kraver90@gmail.com>
2021-11-23 23:13:54 +0500

    add Attach

--- src/test/java/demoqa/test/TestBase.java
+++ src/test/java/demoqa/test/TestBase.java
@@ -1,14 +1,32 @@
 package demoqa.test;
 
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import helpers.Attach;
 
 
 public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
-        Configuration.browser = "Firefox";
+        Configuration.browser = "Chrome";
         Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @AfterEach
+    public void afterEach() {
+        Attach.screenshotAs("Финальный скриншот");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 }
